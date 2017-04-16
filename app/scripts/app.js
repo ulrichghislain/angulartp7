7d48c025368400f0240936fa25a439b187c0a562
Ghislain <ulrichpouansi@gmail.com>
2017-04-16 23:33:23 +0200

    NEW COMMIT

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+angular.module('opowerApp', [
+ 'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap',
+    /*'ui.router',*/
+    'opowerAppService'
+  	])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
