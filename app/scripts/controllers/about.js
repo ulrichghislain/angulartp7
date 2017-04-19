e48f53f03311337a33a4ac74d442360a77f27574
ulrichghislain <ulrichpouansi@gmail.com>
2017-04-19 13:48:47 +0200

    opower

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opowerAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the opowerAppApp
+ */
+angular.module('opowerAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
